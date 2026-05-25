c2f26f280c1912a65c5c950c14f5b5bb0b32cabe
Sendoaa <sendoave@gmail.com>
2026-05-25 12:10:40 +0200

    Chore: initial project structure from spring initializr

---
+++ src/test/java/com/motofix/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motofix.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
